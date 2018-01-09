679b83499864c84bec9ec7fe2fcf0a487da9c994
Kelsey Ruckert <klr324@SCRIM-MAC-LT012.local>
2018-01-09 15:28:08 -0500

    Switch to .netstandard; use nuget

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,10 +1,2 @@
 ﻿using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Landis.Extension.Output.BiomassCommunityCommunity")]
-[assembly: AssemblyDescription("Output Biomass Community Extension")]
-
-
-[assembly: AssemblyVersion("1.0.*")]
